7869c92a0dd6737d6921f5b72cf6f7b9ac2d688b
matheus raposo <rapososilva6@gmail.com>
2025-02-05 16:26:39 -0300

    :tada:initial commit

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    // se tiver light mode, adicionar a imagem light
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+  // se tiver sem light mode, mater a imagem normal
+}
+
+
+// se tiver light mode, adicionar a imagem light
+// se tiver sem light mode, mater a imagem normal
